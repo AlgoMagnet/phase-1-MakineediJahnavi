797e4288fb1294a55c0de24550be4a26173a7fc2
MakineediJahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2021-08-05 18:36:50 +0530

    Update Decision making programs.c

--- Decision making programs.c
+++ Decision making programs.c
@@ -73,3 +73,68 @@ int main()
 Enter three sides of a triangle : 3 4 5
 Triangle is a valid triangle */
 
+/* Determine the type of triangle */
+#include<stdio.h>
+int main()
+{
+	int s1,s2,s3,a,b,c,d,e,f;
+	printf("Enter the sides of triangle : ");
+	scanf("%d%d%d",&s1,&s2,&s3);
+	a=s1*s1; b=s2*s2+s3*s3;
+	c=s2*s2; d=s1*s1+s3*s3;
+	e=s3*s3; f=s1*s1+s2*s2;
+	if(s1==s2&&s1==s3)
+		printf("Triangle is equilateral\n");
+	if(s1==s2 && s1!=s3)
+		printf("Triangle is isosceles\n");
+	if(s1==s3 && s1!=s2)
+		printf("Triangle is isosceles");
+	if(s2==s3 && s2!=s1)
+		printf("Triangle is isosceles\n");	
+	if(s1!=s2 && s2!=s3&&s3!=s1)
+		printf("Triangle is scalene\n");
+	if(a==b || c==d || e==f)
+		printf("Triangle is right angled triangle");
+	return 0; 
+}
+/* OUTPUT:
+Enter the sides of triangle : 3 4 5
+Triangle is scalene
+Triangle is right angled triangle */
+
+/* Converting RGB color to CMYK color format */
+#include<stdio.h>
+int main()
+{
+	float r,g,b,c,m,y,k,w,max;
+	printf("Enter the values of Red, Green, Blue(0 to 255) : ");
+	scanf("%f%f%f",&r,&g,&b);
+	if(r==0&&g==0&&b==0)
+    {
+    	c=m=y=0;
+    	k=1;
+	}
+	else
+	{
+		r=r/255;
+		g=g/255;
+		b=b/255;
+	
+	max=r;
+	if(g>max)
+		max=g;
+	if(b>max)
+		max=b;
+	w=max;
+	c=(w-r)/w;
+	m=(w-g)/w;
+	y=(w-b)/w;
+	k=1-w;
+    }
+	printf("CMYK = %f %f %f %f",c,m,y,k);
+	return 0;
+}
+/* OUTPUT:
+Enter the values of Red, Green, Blue : 255 255 255
+CMYK = 0.000000 0.000000 0.000000 0.000000 */
+
