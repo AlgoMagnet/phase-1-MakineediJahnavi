0990db72e4a7686f9cbc26fd0ad1ba99e7090db6
MakineediJahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2021-08-10 11:31:13 +0530

    Update Decision making programs.c

--- Decision making programs.c
+++ Decision making programs.c
@@ -138,3 +138,61 @@ int main()
 Enter the values of Red, Green, Blue : 255 255 255
 CMYK = 0.000000 0.000000 0.000000 0.000000 */
 
+/* Program to find grade of the steel */
+#include<stdio.h>
+int main()
+{
+	float h,cc,ts;
+	printf("Enter hardness, carbon content, tensile strength of the steel : ");
+	scanf("%f%f%f",&h,&cc,&ts);
+	if(h>50&&cc<0.7&&ts>5600)
+		printf("Grade of the steel is 10");
+	else if(h>50&&cc<0.7&&ts<5600)
+		printf("Grade of the steel is 9");
+	else if(h<50&&cc<0.7&&ts>5600)
+		printf("Grade of the steel is 8");
+	else if(h>50&&cc>0.7&&ts>5600)
+		printf("Grade of the steel is 7");
+	else if(h>50&&cc>0.7&&ts<5600||h<50&&cc<0.7&&ts<5600||h<50&&cc>0.7&&ts>5600)
+		printf("Grade of the steel is 6");
+	else
+		printf("Grade of the steel is 5");
+	return 0;
+}
+/* OUTPUT:
+Enter hardness, carbon content, tensile strength of the steel : 40 0.9 5500
+Grade of the steel is 5 */
+
+/* Program to find BMI Category */
+#include<stdio.h>
+int main()
+{
+	float h,w,b;
+	printf("Enter weight of the person : ");
+	scanf("%f",&w);
+	printf("Enter heigth of the person : ");
+	scanf("%f",&h);
+	b=w/h*h;
+	printf("BMI = %f\n",b);
+	if(b<15)
+		printf("Starvation");
+	else if(b>=15.1&&b<=17.5)
+		printf("Anorexic");
+	else if(b>=17.6&&b<=18.5)
+		printf("Underweight");
+	else if(b>=18.6&&24.9)
+		printf("Ideal");
+	else if(b>=25&&b<=25.9)
+		printf("Overweigth");
+	else if(b>=30&&b<=30.9)
+		printf("Obese");
+	else if(b>=40)
+		printf("Morbidly Obese");
+	return 0;
+}
+/* OUTPUT:
+Enter weight of the person : 65
+Enter height of the prson : 1.6
+BMI = 65.000000
+Ideal */
+
